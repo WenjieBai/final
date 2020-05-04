11046bd728ce37daad9a406f2f2a211d7264524a
Wenjie Bai <bai104@purdue.edu>
2020-05-04 18:55:47 -0400

    666666

--- tcpclient.c
+++ tcpclient.c
@@ -3,8 +3,8 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <sys/socket.h> 
-#define MAX 80 
-#define PORT 8081 
+#define MAX 1040 
+#define PORT 8081
 #define SA struct sockaddr 
 void func(int sockfd) 
 { 
@@ -21,7 +21,7 @@ void func(int sockfd)
 		read(sockfd, buff, sizeof(buff)); 
 		printf("From Server : %s", buff); 
 		if ((strncmp(buff, "exit", 4)) == 0) { 
-			printf("encryption_sideent Exit...\n"); 
+			printf("Client Exit...\n"); 
 			break; 
 		} 
 	} 
@@ -30,7 +30,7 @@ void func(int sockfd)
 int main() 
 { 
 	int sockfd, connfd; 
-	struct sockaddr_in decryption_side, encryption_side; 
+	struct sockaddr_in servaddr, cli; 
 
 	// socket create and varification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -40,15 +40,15 @@ int main()
 	} 
 	else
 		printf("Socket successfully created..\n"); 
-	bzero(&decryption_side, sizeof(decryption_side)); 
+	bzero(&servaddr, sizeof(servaddr)); 
 
 	// assign IP, PORT 
-	decryption_side.sin_family = AF_INET; 
-	decryption_side.sin_addr.s_addr = inet_addr("192.168.15.5"); 
-	decryption_side.sin_port = htons(PORT); 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = inet_addr("192.168.15.5"); 
+	servaddr.sin_port = htons(PORT); 
 
-	// connect the encryption_sideent socket to server socket 
-	if (connect(sockfd, (SA*)&decryption_side, sizeof(decryption_side)) != 0) { 
+	// connect the client socket to server socket 
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
 		printf("connection with the server failed...\n"); 
 		exit(0); 
 	} 
@@ -56,11 +56,12 @@ int main()
 		printf("connected to the server..\n"); 
 
 	// function for chat 
-	// func(sockfd);
-
-    char buff[] = "w cao l ";
+	// func(sockfd); 
+    
+    char buff[MAX] = "qq ni l "; 
     int ret = write(sockfd, buff, sizeof(buff));  
-    fprintf("%s", buff);
+    printf("%s", ret);
 	// close the socket 
 	close(sockfd); 
 } 
+
--- tcpserver.c
+++ tcpserver.c
@@ -5,11 +5,11 @@
 #include <string.h> 
 #include <sys/socket.h> 
 #include <sys/types.h> 
-#define MAX 80 
+#define MAX 1040 
 #define PORT 8081 
 #define SA struct sockaddr 
 
-// Function designed for chat between encryption_sideent and decryption_side. 
+// Function designed for chat between client and server. 
 void func(int sockfd) 
 { 
 	char buff[MAX]; 
@@ -18,22 +18,22 @@ void func(int sockfd)
 	for (;;) { 
 		bzero(buff, MAX); 
 
-		// read the message from encryption_sideent and copy it in buffer 
+		// read the message from client and copy it in buffer 
 		read(sockfd, buff, sizeof(buff)); 
-		// print buffer which contains the encryption_sideent contents 
-		printf("From encryption_sideent: %s\t To encryption_sideent : ", buff); 
+		// print buffer which contains the client contents 
+		printf("From client: %s\t To client : ", buff); 
 		bzero(buff, MAX); 
 		n = 0; 
-		// copy decryption_side message in the buffer 
+		// copy server message in the buffer 
 		while ((buff[n++] = getchar()) != '\n') 
 			; 
 
-		// and send that buffer to encryption_sideent 
+		// and send that buffer to client 
 		write(sockfd, buff, sizeof(buff)); 
 
-		// if msg contains "Exit" then decryption_side exit and chat ended. 
+		// if msg contains "Exit" then server exit and chat ended. 
 		if (strncmp("exit", buff, 4) == 0) { 
-			printf("decryption_side Exit...\n"); 
+			printf("Server Exit...\n"); 
 			break; 
 		} 
 	} 
@@ -43,7 +43,7 @@ void func(int sockfd)
 int main() 
 { 
 	int sockfd, connfd, len; 
-	struct sockaddr_in servaddr, encryption_side; 
+	struct sockaddr_in servaddr, client; 
 
 	// socket create and verification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -68,35 +68,33 @@ int main()
 	else
 		printf("Socket successfully binded..\n"); 
 
-	// Now decryption_side is ready to listen and verification 
+	// Now server is ready to listen and verification 
 	if ((listen(sockfd, 5)) != 0) { 
 		printf("Listen failed...\n"); 
 		exit(0); 
 	} 
 	else
-		printf("decryption_side listening..\n"); 
+		printf("Server listening..\n"); 
+	len = sizeof(client); 
 
-	len = sizeof(encryption_side); 
-
-	// Accept the data packet from encryption_sideent and verification 
-	connfd = accept(sockfd, (SA*)&encryption_side, &len); 
+	// Accept the data packet from client and verification 
+	connfd = accept(sockfd, (SA*)&client, &len); 
 	if (connfd < 0) { 
-		printf("decryption_side acccept failed...\n"); 
+		printf("server acccept failed...\n"); 
 		exit(0); 
 	} 
 	else
-		{
-            printf("decryption_side acccept the encryption_sideent...\n"); 
-            printf("new fd %d\n", connfd);
-        }
+		printf("server acccept the client...\n"); 
 
-	// Function for chatting between encryption_sideent and decryption_side 
-    char buff[80];
-    int ret = read(sockfd, buff, sizeof(buff));
-    printf("From encryption_sideent: %s\t To encryption_sideent : \n", buff); 
-    printf("ret %d", ret);
+	// Function for chatting between client and server 
+	// func(connfd); 
+
+    char buff[MAX]; 
+    read(sockfd, buff, sizeof(buff)); 
+    // print buffer which contains the client contents 
+    printf("From client: %s\t : ", buff); 
+
 	// After chatting close the socket 
-	close(sockfd);
-     
+	close(sockfd); 
 } 
 
